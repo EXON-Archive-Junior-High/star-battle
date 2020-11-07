4a923bbef9f57a311f7aa36e26824add41c75f73
시혁 박 <exon.indiv@gmail.com>
2020-11-07 18:29:32 +0900

    design

--- star-battle/src/App.tsx
+++ star-battle/src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+            <div className="head">
+                <h1>Github Star Battle</h1>
+                <p>깃허브 스타 대결 사이트!</p>
+            </div>
+            <div className="main">
+                <input></input>
+                <button>검색</button>
+            </div>
+        </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
